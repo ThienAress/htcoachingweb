4ca73a2a65a661c90cad19fe92b35d03ed652212
Hoang Thien Vo <hoangthiengym1999@gmail.com>
2026-06-01 00:18:43 +0700

    style: update ClassCard to use brand primary color

--- client/src/sections/class/Classes.jsx
+++ client/src/sections/class/Classes.jsx
@@ -45,7 +45,7 @@ const Classes = ({ images }) => {
   ];
 
   return (
-    <section id="classes" className="py-12 sm:py-16">
+    <section id="classes" className="py-12 sm:py-16 ">
       <div className="mx-auto w-full max-w-[1650px] px-4 sm:px-6 xl:px-8">
         <h2 className="text-center text-primary text-2xl sm:text-3xl md:text-4xl lg:text-5xl uppercase"
           data-aos="fade-down"
